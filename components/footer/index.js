e7328658e8f9c8e668f66cd5fe26a456b6357c20
Liam Klyneker <liamklyneker@gmail.com>
2020-03-27 16:09:46 -0500

    feat(footer-component): add footer component

---
+++ components/footer/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <a href="#">Edit this page with Tina</a>
+    </footer>
+  )
+}
+
+export default Footer
--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,21 @@
 import React from "react"
 
-import TopBar from './topbar';
+import TopBar from './topbar'
+import Footer from './footer'
 
 export default function layout({ children }) {
   return (
     <main>
       <TopBar />
-      {children}
+      <div className="body">
+        {children}
+      </div>
+      <Footer />
       <style jsx>{`
         :global(body) {
           margin: 0;
           font-family: Roboto, system-ui, sans-serif;
+          max
         }
         nav {
           text-align: center;
@@ -30,6 +35,9 @@ export default function layout({ children }) {
           color: #067df7;
           text-decoration: none;
           font-size: 13px;
+        }
+        .body {
+          min-height: calc(100vh - 160px)
         }
       `}</style>
     </main>
