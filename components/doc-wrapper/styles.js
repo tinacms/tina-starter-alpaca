8829342d5b44c54ccc59321b74d739635a3146a6
GeraldM <gerald@bejamas.io>
2020-04-17 14:59:04 -0600

    feat: add max-width to image in edit state

--- components/doc-wrapper/styles.js
+++ components/doc-wrapper/styles.js
@@ -4,4 +4,11 @@ export const DocWrapperStyled = styled.section`
   flex: 1;
   max-width: 762px;
   margin-left: 0;
+
+  .wysiwyg-wrapper {
+    & img {
+      max-width: 100%;
+      width: 100%;
+    }
+  }
 `
