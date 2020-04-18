c8b25ded6d21718a406585d0c501540f43d90748
GeraldM <gerald@bejamas.io>
2020-04-17 22:54:06 -0600

    feat: add support to upload images in InlineWysiwyg

--- pages/docs/[...slug].js
+++ pages/docs/[...slug].js
@@ -15,41 +15,62 @@ import Toc from "@components/Toc"
 import { parseNestedDocsMds, flatDocs, createToc } from "@utils"
 import { useFormEditDoc, useCreateChildPage } from "@hooks"
 
+import { useCMS } from "tinacms"
 import { InlineForm, InlineTextField, InlineWysiwyg } from "react-tinacms-inline"
 import InlineEditingControls from "@components/inline-controls"
 
 const DocTemplate = ({ markdownFile, allNestedDocs, Alltocs }) => {
   const router = useRouter()
+  const cms = useCMS()
+
   useCreateChildPage(allNestedDocs)
   const [data, form] = useFormEditDoc(markdownFile)
+
+  if (!form) return null
   return (
-    <>
+    <Layout showDocsSearcher splitView>
       <Head title={data.frontmatter.title} />
-      <Layout showDocsSearcher splitView>
+      <InlineForm form={form}>
         <SideNav
           allNestedDocs={allNestedDocs}
           currentSlug={router.query.slug}
           groupIn={data.frontmatter.groupIn}
         />
         <DocWrapper>
-          <InlineForm form={form}>
-            {process.env.NODE_ENV !== "production" && <InlineEditingControls />}
+          {process.env.NODE_ENV !== "production" && <InlineEditingControls />}
+          <main>
+            <h1>
+              <InlineTextField name="frontmatter.title" />
+            </h1>
+            {Alltocs.length > 0 && <Toc tocItems={Alltocs} />}
+            <InlineWysiwyg
+              imageProps={{
+                async upload(files) {
+                  const directory = "/public/images/"
 
-            <main>
-              <h1>
-                <InlineTextField name="frontmatter.title" />
-              </h1>
-              {Alltocs.length > 0 && <Toc tocItems={Alltocs} />}
-              <InlineWysiwyg name="markdownBody">
-                <MarkdownWrapper source={data.markdownBody} />
-              </InlineWysiwyg>
-            </main>
-          </InlineForm>
+                  let media = await cms.media.store.persist(
+                    files.map((file) => {
+                      return {
+                        directory,
+                        file,
+                      }
+                    })
+                  )
+                  return media.map((m) => `/images/${m.filename}`)
+                },
+                previewUrl: (str) => str,
+              }}
+              name="markdownBody"
+            >
+              <MarkdownWrapper source={data.markdownBody} />
+            </InlineWysiwyg>
+          </main>
+
           <PostNavigation allNestedDocs={allNestedDocs} />
           <PostFeedback />
         </DocWrapper>
-      </Layout>
-    </>
+      </InlineForm>
+    </Layout>
   )
 }
 
