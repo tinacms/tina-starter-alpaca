b45dbbe4b0606be112787338032d2142e9e1f334
Logan Anderson <landerson2@upei.ca>
2020-05-21 13:19:32 -0300

    creating with groups now works

--- hooks/useCreateChildPage.js
+++ hooks/useCreateChildPage.js
@@ -3,10 +3,25 @@ import { useRouter } from "next/router"
 
 import { toMarkdownString } from "@utils"
 
-const useCreateChildPage = (allDocs) => {
+const useCreateChildPage = async (allDocs) => {
   const router = useRouter()
   const cms = useCMS()
+  const category = router.query.slug[0]
   const parentObject = allDocs.find((item) => item.key === router.query.slug[0])
+
+  // // find all the groups
+  const groups = []
+  allDocs.forEach((doc) => {
+    // if the doc is the the catorgory we are clicking on
+    if (doc.slug.startsWith(category)) {
+      doc.children.forEach((childDoc) => {
+        if (childDoc.type === "group") {
+          groups.push(childDoc.title)
+        }
+      })
+    }
+  }, [])
+
   //TODO: in preview mode will have to query github for this
   usePlugins([
     {
@@ -25,35 +40,53 @@ const useCreateChildPage = (allDocs) => {
           component: "text",
           required: true,
         },
-        // {
-        //   name: "groupIn",
-        //   label: "Group in",
-        //   description: "Group under a name to create a 3rd level",
-        //   component: "text",
-        // },
+        {
+          name: "groupIn",
+          label: "Group in",
+          description: "Group under a name to create a 3rd level",
+          component: "select",
+          options: ["No Group", ...groups],
+        },
       ],
       onSubmit: async ({ slug, title, groupIn }) => {
-        const fileRelativePath = `docs/${router.query.slug[0]}/${slug}.md`
-        const category = router.query.slug[0]
+        // get confile JSON file from github
         const configFile = await cms.api.github.fetchFile("docs/config.json", null)
-        const sha = configFile.sha
         const allNestedDocsRemote = JSON.parse(configFile.decodedContent)
+        const category = router.query.slug[0]
+        const fileRelativePath = `docs/${router.query.slug[0]}/${slug}.md`
+        const sha = configFile.sha
 
         console.log(allNestedDocsRemote)
+
+        const defaultItem = {
+          type: "link",
+          key: slug,
+          slug: `${category}/${slug}`,
+          title,
+          position: null,
+          children: [],
+        }
+
+        // find the current category and add it to it
         allNestedDocsRemote.config.forEach((element) => {
           if (element.slug.startsWith(category)) {
-            element.children.unshift({
-              type: "link",
-              key: slug,
-              slug: `${category}/${slug}`,
-              title,
-              position: null,
-              children: [],
-            })
+            if (groupIn === "No Group") {
+              // not adding it to a third level group
+              element.children.unshift(defaultItem)
+            } else {
+              // we are adding it to a third level group
+              // find the group
+              element.children.forEach((child) => {
+                if (child.type === "group" && child.title === groupIn) {
+                  // we found the group now add a new child that links to the new doc
+                  child.children.unshift(defaultItem)
+                }
+              })
+            }
           }
         })
-        console.log(allNestedDocsRemote)
 
+        // commit new json file to github
         await cms.api.github.commit(
           "docs/config.json",
           sha,
--- utils/flatDocs.js
+++ utils/flatDocs.js
@@ -2,4 +2,7 @@ export default (docs) =>
   docs.reduce((newArray, item) => [...newArray, ...[item], ...flatChilds(item.children)], [])
 
 const flatChilds = (docs) =>
-  docs.reduce((a, b) => a.concat(b.children ? flatChilds(b.children) : b), [])
+  docs.reduce(
+    (a, b) => a.concat(b.children && b.children.length > 0 ? flatChilds(b.children) : b),
+    []
+  )
