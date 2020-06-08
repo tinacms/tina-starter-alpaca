ff52f6618a6e7acdc73fc8589523ddb6417c0694
Logan Anderson <landerson2@upei.ca>
2020-06-08 14:15:59 -0300

    fix: added files

---
+++ pages/api/feedback.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const username = process.env.USERNAME_ISSUES
+    const password = process.env.GITHUB_ACCESS_TOKEN
+    const repo = process.env.REPO_ISSUES
+
+    if (!username || !password || !repo) {
+      res
+        .status(501)
+        .json({ message: "Username, password and repo must be added to the .env file" })
+    }
+    const token = Buffer.from(`${username}:${password}`, "utf8").toString("base64")
+    const url = `https://api.github.com/repos/${username}/${repo}/issues`
+
+    const response = await axios({
+      url,
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
+      data: {
+        title: `Comment from documentation starter: page ${req.body.location}`,
+        body: JSON.stringify(req.body, null, 2),
+        labels: [req.body.location, req.body.reaction],
+      },
+    })
+    res.status(200).json({ message: "sent" })
+  } else {
+    // Handle any other HTTP method
+    res.status(400).json({ message: "only post methods supported" })
+  }
+}
